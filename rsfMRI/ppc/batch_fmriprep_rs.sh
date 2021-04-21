ae97ca3f8c7521ed0ce11fb32cbb386a17c8071d
Ben Smith <benjsmith@gmail.com>
2021-04-20 21:33:53 -0700

    all

---
+++ rsfMRI/ppc/batch_fmriprep_rs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# This batch file calls on your subject list (which contains both ID and wave number: SID000,wave1). 
+# And runs the job_fmriprep.sh file for each subject. 
+# It saves the ouput and error files in specified directories.
+#
+# Set your directories
+
+container=containers/fmriprep-latest-2018-09-05.simg
+freesurferlicense=/projects/sanlab/shared/containers/license.txt
+group_dir=/projects/sanlab/shared/ #set path to directory within which study folder lives
+study=DEV
+study_dir="${group_dir}""${study}"
+output_dir="${study_dir}"/"${study}"_scripts/rsfMRI/ppc/output
+
+if [ ! -d "${output_dir}" ]; then
+    mkdir -v "${output_dir}"
+fi
+
+
+# Set subject list
+subject_list=`cat subject_list_0419.txt` 
+
+pre_subid=""
+pre_sessionid=""
+
+# Loop through subjects and run job_mriqc
+for subject in $subject_list; do
+  #dcmfolder=`echo $subject|awk '{print $1}' FS=","`
+    #this is obsolete
+	subid=`echo $subject | awk -F ',' '{print $1}'`
+	sessid=`echo $subject | awk -F ',' '{print $2}'`
+	echo $subid, $sessid
+	if [ "$subid" == "$pre_subid" ] && [ "$sessid" == "$pre_sessionid" ]; then
+	    echo "same session"
+	    continue
+	fi
+	prepOutput_dir="${study_dir}"/bids_data/rs_derivatives/fmriprep/sub-"$subid"/ses-"$sessid"/func
+	[ -d "${prepOutput_dir}" ] && { prep_files=$(find $prepOutput_dir -name "*acq-1*" | wc -l)
+	if [ "$prep_files" == 5 ]; then
+		echo "skip"
+		continue
+	fi; }
+	sbatch --export ALL,subid=${subid},sessid=${sessid},group_dir=${group_dir},study_dir=${study_dir},study=${study},container=${container},freesurferlicense=${freesurferlicense} \
+		   --job-name fmriprep_rs \
+		   --partition=short \
+		   --cpus-per-task=8 \
+		   --mem=10G \
+		   --time=10:00:00 \
+		   -o "${output_dir}"/"${subid}"_"${sessid}"_fmriprep_rs_output.txt \
+		   -e "${output_dir}"/"${subid}"_"${sessid}"_fmriprep_rs_error.txt \
+		   --account=sanlab \
+		   job_fmriprep_rs.sh
+    pre_subid=$subid
+    pre_sessionid=$sessid
+done
