e8e9cda73e82ca8ec6febbf4e6d258cb01989fb0
Ben J. Smith <benjsmith@gmail.com>
2022-03-03 13:28:44 +1300

    attempting to do SPM l2 analysis

---
+++ fMRI/fx/models/SST/level2/scratch/mlr_ssrt_subject_average.m
@@ -0,0 +1,22 @@
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/Users/benjaminsmith/Google Drive/oregon/preprocessing/DEV_scripts/fMRI/fx/models/SST/level2'};
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.scans = {
+                                                            '/Users/benjaminsmith/Google Drive/oregon/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/conditions/sub-DEV005/beta_0002.nii,1'
+                                                            '/Users/benjaminsmith/Google Drive/oregon/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/conditions/sub-DEV006/beta_0002.nii,1'
+                                                            '/Users/benjaminsmith/Google Drive/oregon/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/conditions/sub-DEV009/beta_0002.nii,1'
+                                                            '/Users/benjaminsmith/Google Drive/oregon/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/conditions/sub-DEV010/beta_0002.nii,1'
+                                                            };
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov.c = [1
+                                                             2.9
+                                                             3.8
+                                                             0.3];
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov.cname = 'SSRT';
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov.iCC = 1;
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.incint = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
