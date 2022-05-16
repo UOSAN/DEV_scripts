98751203015aa8ef708ef5f50d54528ea030baad
Ben Smith <benjsmith@gmail.com>
2022-05-16 12:06:44 -0700

    may 2 bad file, more trying tofigure out post-error data

---
+++ fMRI/fx/models/SST/level2/correctgo_following_failed_and_correct_stop/attempt3_CS_first/CGFollowing(CS-FS)/CGFollowing(CS-FS)_contrast_one_sample_design_estimate.m
@@ -0,0 +1,99 @@
+%-----------------------------------------------------------------------
+% Job saved on 16-Mar-2022 09:47:13 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/Users/benjaminsmith/Google Drive/oregon/code/DEV_scripts/fMRI/fx/models/SST/level2/correctgo_following_failed_and_correct_stop/CGFollowing(CS-FS)/'};
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = {
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV005/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV006/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV010/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV011/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV013/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV014/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV015/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV016/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV017/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV018/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV019/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV020/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV021/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV022/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV023/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV024/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV026/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV027/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV028/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV029/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV030/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV034/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV035/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV036/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV039/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV040/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV041/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV042/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV043/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV046/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV047/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV048/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV049/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV050/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV051/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV052/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV053/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV055/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV056/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV057/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV058/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV059/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV060/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV062/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV064/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV067/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV068/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV069/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV071/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV073/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV074/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV076/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV077/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV079/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV083/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV084/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV085/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV086/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV087/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV089/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV097/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV105/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV107/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV135/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV141/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV145/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV157/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV158/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV159/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV164/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV169/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV171/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV173/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV177/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV186/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV197/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV206/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV215/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV216/spmT_0001.nii,1'
+'/Users/bensmith/Documents/data/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_conditions_repeat3-CS-first/sub-DEV217/spmT_0001.nii,1'
+                                                          };
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
