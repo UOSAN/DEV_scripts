9e788489865ac206b5fdcc9c1353c0623b88d0c9
Ben Smith <benjsmith@gmail.com>
2022-05-26 13:29:22 -0700

    posterror

--- fMRI/fx/models/SST/batch_spm_job_fx_posterror_conditions_w_pss.sh
+++ fMRI/fx/models/SST/batch_spm_job_fx_posterror_conditions_w_pss.sh
@@ -28,9 +28,8 @@ TASK=SST
 #SUBJ_NO_CONTRAST=`cat ${SCRIPTS_DIR}/fMRI/fx/multiconds/$TASK/full_duration/posterror_conditions/missing_1.txt`
 
 # Set MATLAB script path
-#SCRIPT_W_CONTRASTS=${SCRIPTS_DIR}/fMRI/fx/models/$TASK/fx_posterror_conditions_w1_w_contrasts.m
-#SCRIPT_NO_CONTRASTS=${SCRIPTS_DIR}/fMRI/fx/models/$TASK/fx_posterror_conditions_w1.m
-SCRIPT=${SCRIPTS_DIR}/fMRI/fx/models/$TASK/fx_posterror_conditions_w1_w_pss.m
+SCRIPT_W_CONTRASTS=${SCRIPTS_DIR}/fMRI/fx/models/$TASK/fx_posterror_conditions_w1_w_pss.m
+SCRIPT_NO_CONTRASTS=${SCRIPTS_DIR}/fMRI/fx/models/$TASK/fx_posterror_conditions_w1.m
 
 
 # Set shell script to execute
@@ -54,29 +53,9 @@ mempercpu=8G
 # I don't think there separate contrast/non-contrast analyses here?
 for SUB in $SUBJLIST; do
 	echo $SUB
-# 	if [[ " ${SUBJ_TO_CONTRAST[*]} " =~ "${SUB}" ]]; then
-#     	echo "subject $SUB should have a contrast; running $SCRIPT_W_CONTRASTS"
-#     	sbatch --export ALL,REPLACESID=$REPLACESID,SCRIPT=$SCRIPT_W_CONTRASTS,SUB=$SUB,SPM_PATH=$SPM_PATH,  \
-# 		 	--job-name=${RESULTS_INFIX} \
-# 		 	-o ${OUTPUTDIR}/${SUB}_${RESULTS_INFIX}.log \
-# 		 	--cpus-per-task=${cpuspertask} \
-# 		 	--mem-per-cpu=${mempercpu} \
-# 		 	--account=sanlab \
-# 		 	${SHELL_SCRIPT}
-# 	 	sleep .25
-#     elif [[ " ${SUBJ_NO_CONTRAST[*]} " =~ "${SUB}" ]]; then
-#     	echo "subject $SUB should NOT have a contrast; running $SCRIPT_NO_CONTRASTS"
-#     	sbatch --export ALL,REPLACESID=$REPLACESID,SCRIPT=$SCRIPT_NO_CONTRASTS,SUB=$SUB,SPM_PATH=$SPM_PATH,  \
-# 		 	--job-name=${RESULTS_INFIX} \
-# 		 	-o ${OUTPUTDIR}/${SUB}_${RESULTS_INFIX}.log \
-# 		 	--cpus-per-task=${cpuspertask} \
-# 		 	--mem-per-cpu=${mempercpu} \
-# 		 	--account=sanlab \
-# 		 	${SHELL_SCRIPT}
-# 	 	sleep .25
-#     else
-#     	echo "warning for subject $SUB: not in contrast or not contrast list. first-level analysis WITHOUT contrast will be attempted using $SCRIPT_NO_CONTRASTS."
-    	sbatch --export ALL,REPLACESID=$REPLACESID,SCRIPT=$SCRIPT,SUB=$SUB,SPM_PATH=$SPM_PATH,  \
+	if [[ " ${SUBJ_TO_CONTRAST[*]} " =~ "${SUB}" ]]; then
+    	echo "subject $SUB should have a contrast; running $SCRIPT_W_CONTRASTS"
+    	sbatch --export ALL,REPLACESID=$REPLACESID,SCRIPT=$SCRIPT_W_CONTRASTS,SUB=$SUB,SPM_PATH=$SPM_PATH,  \
 		 	--job-name=${RESULTS_INFIX} \
 		 	-o ${OUTPUTDIR}/${SUB}_${RESULTS_INFIX}.log \
 		 	--cpus-per-task=${cpuspertask} \
@@ -84,7 +63,27 @@ for SUB in $SUBJLIST; do
 		 	--account=sanlab \
 		 	${SHELL_SCRIPT}
 	 	sleep .25
-# 	fi
-# 	
+    elif [[ " ${SUBJ_NO_CONTRAST[*]} " =~ "${SUB}" ]]; then
+    	echo "subject $SUB should NOT have a contrast; running $SCRIPT_NO_CONTRASTS"
+    	sbatch --export ALL,REPLACESID=$REPLACESID,SCRIPT=$SCRIPT_NO_CONTRASTS,SUB=$SUB,SPM_PATH=$SPM_PATH,  \
+		 	--job-name=${RESULTS_INFIX} \
+		 	-o ${OUTPUTDIR}/${SUB}_${RESULTS_INFIX}.log \
+		 	--cpus-per-task=${cpuspertask} \
+		 	--mem-per-cpu=${mempercpu} \
+		 	--account=sanlab \
+		 	${SHELL_SCRIPT}
+	 	sleep .25
+    else
+    	echo "warning for subject $SUB: not in contrast or not contrast list. first-level analysis WITHOUT contrast will be attempted using $SCRIPT_NO_CONTRASTS."
+    	sbatch --export ALL,REPLACESID=$REPLACESID,SCRIPT=$SCRIPT_NO_CONTRASTS,SUB=$SUB,SPM_PATH=$SPM_PATH,  \
+		 	--job-name=${RESULTS_INFIX} \
+		 	-o ${OUTPUTDIR}/${SUB}_${RESULTS_INFIX}.log \
+		 	--cpus-per-task=${cpuspertask} \
+		 	--mem-per-cpu=${mempercpu} \
+		 	--account=sanlab \
+		 	${SHELL_SCRIPT}
+	 	sleep .25
+	fi
+	
 done
 
