5e57be2a90e5405a444a60cbf27214721fdaa650
Ben Smith <benjsmith@gmail.com>
2022-08-19 17:04:05 -0700

    add recent l3 changes

---
+++ fMRI/fx/multiconds/SST/run_multiconds_bensmith_labmac.sh
@@ -0,0 +1,1 @@
+python3 multiconds_s_187.py --input="/Users/benjaminsmith/Dropbox (University of Oregon)/UO-SAN Lab/Berkman Lab/Devaluation/Tasks/SST_DEV/output" --bids=/Users/benjaminsmith/Desktop/DEV/scratchpad/bids_data
