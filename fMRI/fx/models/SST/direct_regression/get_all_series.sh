69100401675fd9dbfed6cf9118bf9f0787de3b3a
Ben Smith <benjsmith@gmail.com>
2022-09-30 14:51:10 -0700

    latest direct regression stuff

--- fMRI/fx/models/SST/direct_regression/get_all_series.sh
+++ fMRI/fx/models/SST/direct_regression/get_all_series.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-#SBATCH --partition=interactive
+#SBATCH --partition=ctn
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
@@ -8,8 +7,10 @@
 #SBATCH --time 0-4:00:00
 #SBATCH --account=sanlab
 
+echo "loading anaconda"
 module load anaconda3
 conda activate neuralsignature
-python get_all_series.py
+python get_all_series_main.py
 
-#sbatch -o get_all_series_log --account=sanlab get_all_series.py
+echo "finished"
+#sbatch -o get_all_series_log.out get_all_series_main.py
--- fMRI/fx/models/SST/direct_regression/run_regression.sh
+++ fMRI/fx/models/SST/direct_regression/run_regression.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-#SBATCH --partition=interactive
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=20G
-#SBATCH --time 0-4:00:00
-#SBATCH --job-name jupyter
-#SBATCH --output jupyter-%J.log
-#SBATCH --account=sanlab
+sbatch -o get_all_series_log --account=sanlab get_all_series.sh
 
 
-sbatch -o get_all_series_log --account=sanlab get_all_series.py
