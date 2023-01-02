f7d949cec7010dc9abca862870796b512d7fbb4a
Ben J. Smith <benjsmith@gmail.com>
2023-01-02 09:32:34 -0800

    fix yaml

--- fMRI/ml/dev_utils.py
+++ fMRI/ml/dev_utils.py
@@ -3,6 +3,11 @@ import yaml
 
 def read_yaml_for_host(file_path):
     hostname = socket.gethostname()
-    with open(file_path, "r") as f:
-        return yaml.safe_load(f)[hostname]
-
+    yaml_full = yaml.safe_load(f)
+    if hostname in yaml_full:
+        yaml_host = yaml_full[hostname]
+    else:
+        print("yaml file does not contain host-specific settings for this host. Using default settings.")
+        yaml_host = yaml_full['default']
+    
+    return(yaml_host)
