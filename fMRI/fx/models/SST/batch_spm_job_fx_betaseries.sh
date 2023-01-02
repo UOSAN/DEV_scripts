c965fa44578d8bcda30edb76874ca3cdce79743f
Ben J. Smith <benjsmith@gmail.com>
2023-01-02 10:26:18 -0800

    update the betaseries to get all wave 1 SST

--- fMRI/fx/models/SST/batch_spm_job_fx_betaseries.sh
+++ fMRI/fx/models/SST/batch_spm_job_fx_betaseries.sh
@@ -9,7 +9,7 @@
 STUDY=DEV
 
 # Set subject list
-SUBJLIST=`cat subject_list_single_col_20210908.txt`
+SUBJLIST=`cat subject_list_single_col_wave1.txt`
 
 # Which SID should be replaced?
 REPLACESID=DEV004
