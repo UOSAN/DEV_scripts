fe3d1e460b4bf3fef84cf3e6f859b56ffe1a859c
Ben Smith <benjsmith@gmail.com>
2023-01-03 12:01:27 -0800

    trialwise changes

--- fMRI/ml/SST/classify_trialwise_gng.py
+++ fMRI/ml/SST/classify_trialwise_gng.py
@@ -50,7 +50,7 @@ def main(normalize_across_features=True,screening_percentile=5):
 
     #brain_data_filepath = ml_data_folderpath + '/SST/Brain_Data_betaseries_40subs_correct_cond.pkl'
     #brain_data_filepath = ml_data_folderpath + '/SST/Brain_Data_betaseries_15subs_correct_cond.pkl'
-    brain_data_filepath = ml_data_folderpath + '/SST/Brain_Data_betaseries_30subs_correct_cond_pfc.pkl'
+    brain_data_filepath = ml_data_folderpath + '/SST/Brain_Data_betaseries_30subs_correct_cond_pfc_condstd.pkl'
     print(brain_data_filepath)
     warnings.warn("not sure if this file holds up--it was created in 2021; need to see if it's still valid")
     train_test_markers_filepath = ml_data_folderpath + "/train_test_markers_20220818T144138.csv"
