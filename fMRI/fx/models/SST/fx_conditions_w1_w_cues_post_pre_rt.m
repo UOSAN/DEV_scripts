590d95f7ccf5bc4a35f0de1b92a488597a4b7eb6
Ben J. Smith <benjsmith@gmail.com>
2023-01-19 10:11:04 -0800

    update first-level dirs

--- fMRI/fx/models/SST/fx_conditions_w1_w_cues_post_pre_rt.m
+++ fMRI/fx/models/SST/fx_conditions_w1_w_cues_post_pre_rt.m
@@ -3,13 +3,13 @@
 % spm SPM - SPM12 (7771)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-conditions_filepath = '/projects/sanlab/shared/DEV/DEV_scripts/fMRI/fx/multiconds/SST/full_duration/posterror_cues_no_rt/DEV004_1_SST1.mat';
+conditions_filepath = '/projects/sanlab/shared/DEV/DEV_scripts/fMRI/fx/multiconds/SST/full_duration/posterror_cues_with_drt/DEV004_1_SST1.mat';
 matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/projects/sanlab/shared/DEV/bids_data/derivatives/fmriprep/sub-DEV004/ses-wave1/func/'};
 matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.filter = 's6_sub-DEV004_ses-wave1_task-SST_acq-1_bold_space-MNI152NLin2009cAsym_preproc.nii';
 matlabbatch{1}.cfg_basicio.file_dir.file_ops.file_fplist.rec = 'FPList';
 matlabbatch{2}.spm.util.exp_frames.files(1) = cfg_dep('File Selector (Batch Mode): Selected Files (s6_sub-DEV004_ses-wave1_task-SST_acq-1_bold_space-MNI152NLin2009cAsym_preproc.nii)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
 matlabbatch{2}.spm.util.exp_frames.frames = Inf;
-matlabbatch{3}.spm.stats.fmri_spec.dir = {'/projects/sanlab/shared/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_cues_no_rt/sub-DEV004'};
+matlabbatch{3}.spm.stats.fmri_spec.dir = {'/projects/sanlab/shared/DEV/nonbids_data/fMRI/fx/models/SST/wave1/posterror_cues_with_drt/sub-DEV004'};
 matlabbatch{3}.spm.stats.fmri_spec.timing.units = 'secs';
 matlabbatch{3}.spm.stats.fmri_spec.timing.RT = 2;
 matlabbatch{3}.spm.stats.fmri_spec.timing.fmri_t = 72;
