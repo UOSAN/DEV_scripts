d2ffc2983503551616f021f71ab9f82ccaedc593
Ben J. Smith <benjsmith@gmail.com>
2023-04-16 15:44:28 -0700

    fixing dicoms gain

--- org/dicom_check/dicom_correct#OnDisk.py
+++ org/dicom_check/dicom_correct#OnDisk.py
@@ -22,9 +22,8 @@ print_and_log("Running modification script at " + str(datetime.datetime.now()))
 #read in the csv file, setting a custom header since there is none in the csv
 df_runs = pd.read_csv('runs_to_reset.csv', header=None, names=['subid', 'runid'])
 
-bids_path = '/gpfs/projects/sanlab/shared/DEV/bids_data/'
-#bids_path =  '/Users/benjaminsmith/Google Drive/oregon/data/DEV/bids_data/'
-
+#bids_path = '/gpfs/projects/sanlab/shared/DEV/bids_data/'
+bids_path =  '/Users/benjaminsmith/Google Drive/oregon/data/DEV/bids_data/'
 
 for row in df_runs.itertuples():
     #get the subject id and run id
@@ -33,30 +32,23 @@ for row in df_runs.itertuples():
     
     #get the filepath to the nifti file
     img_folder = bids_path + f'sub-{subid}/ses-{runid}/*/'
-    
 
     #get a list of the nifti files in the folder
-    # if os.path.exists(img_folder):
-    #     #img_files = [f for f in os.listdir(img_folder) if f.endswith('.nii.gz')]
-    img_files = [f for f in glob.glob(img_folder + "*.nii.gz")]
-    img_files = [i for i in img_files if 'func' not in i] #exclude functionals
-    
-    if len(img_files) == 0:
+    if os.path.exists(img_folder):
+        #img_files = [f for f in os.listdir(img_folder) if f.endswith('.nii.gz')]
+        img_files = [f for f in glob.glob(img_folder + "*.nii.gz")]
+    else:
         print_and_log(f'No folder found for sub-{subid} run-{runid}')
         continue
-
-    
     #print the number of images found.
     print_and_log(f'Found {len(img_files)} images for sub-{subid} run-{runid}')
 
 
-
     #make a backup folder
     backup_folder = img_folder.replace('bids_data/',f'bids_data/derivatives/affine_correction_originals/')
-    for bf in ['func','anat','fmap']:
-        run_backup_folder = backup_folder + f'sub-{subid}/ses-{runid}/' + bf + '/'
-        if not os.path.exists(run_backup_folder):
-            os.makedirs(run_backup_folder)
+    run_backup_folder = backup_folder + f'sub-{subid}/ses-{runid}/func/'
+    if not os.path.exists(run_backup_folder):
+        os.makedirs(run_backup_folder)
 
     #loop through the nifti files
     for img_filepath in img_files:
--- org/dicom_check/dicom_correct.py
+++ org/dicom_check/dicom_correct.py
@@ -23,7 +23,7 @@ print_and_log("Running modification script at " + str(datetime.datetime.now()))
 df_runs = pd.read_csv('runs_to_reset.csv', header=None, names=['subid', 'runid'])
 
 bids_path = '/gpfs/projects/sanlab/shared/DEV/bids_data/'
-
+#bids_path =  '/Users/benjaminsmith/Google Drive/oregon/data/DEV/bids_data/'
 
 for row in df_runs.itertuples():
     #get the subject id and run id
@@ -31,12 +31,13 @@ for row in df_runs.itertuples():
     runid = row.runid
     
     #get the filepath to the nifti file
-    img_folder = bids_path + f'sub-{subid}/ses-{runid}/func/'
+    img_folder = bids_path + f'sub-{subid}/ses-{runid}/*/'
 
     #get a list of the nifti files in the folder
-    if os.path.exists(img_folder):
-        img_files = [f for f in os.listdir(img_folder) if f.endswith('.nii.gz')]
-    else:
+    #if os.path.exists(img_folder):
+        #img_files = [f for f in os.listdir(img_folder) if f.endswith('.nii.gz')]
+    img_files = [f for f in glob.glob(img_folder + "*.nii.gz")]
+    if len(img_files) == 0:
         print_and_log(f'No folder found for sub-{subid} run-{runid}')
         continue
     #print the number of images found.
@@ -45,9 +46,10 @@ for row in df_runs.itertuples():
 
     #make a backup folder
     backup_folder = img_folder.replace('bids_data/',f'bids_data/derivatives/affine_correction_originals/')
-    run_backup_folder = backup_folder + f'sub-{subid}/ses-{runid}/func/'
-    if not os.path.exists(run_backup_folder):
-        os.makedirs(run_backup_folder)
+    for bf in ['func','anat','fmap']:
+        run_backup_folder = backup_folder + f'sub-{subid}/ses-{runid}/' + bf + '/'
+        if not os.path.exists(run_backup_folder):
+            os.makedirs(run_backup_folder)
 
     #loop through the nifti files
     for img_filepath in img_files:
