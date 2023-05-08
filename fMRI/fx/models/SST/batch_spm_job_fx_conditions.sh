1b53ab6078a1c9dd4cbd4c7b4bb33aa3c3ede9c1
Ben J. Smith <benjsmith@gmail.com>
2023-05-08 11:52:47 -0700

    update to batch files

--- fMRI/fx/models/SST/batch_spm_job_fx_conditions.sh
+++ fMRI/fx/models/SST/batch_spm_job_fx_conditions.sh
@@ -9,7 +9,7 @@
 STUDY=DEV
 
 # Set subject list
-SUBJLIST=`cat subject_list_single_col_wave1.txt`
+SUBJLIST=`cat subject_list_single_col_20230210.txt`
 
 # Which SID should be replaced?
 REPLACESID=004
