3ec6fa6c448e67119ed2e65385f08e4c3405fb97
Denicia Aragon <denicia@UO-2010765>
2023-08-16 12:30:49 -0700

    Update HealthyDisliked(T2<T1)_one_sample_design_estimate.m, HealthyDisliked(T2>T1)_one_sample_design_estimate.m, and 7 more files...

---
+++ fMRI/fx/models/WTP/level2/prepostWTP_20230815/HealthyVSUnhealthy/HealthyVSUnhealthy_one_sample_design_estimate.m
@@ -0,0 +1,38 @@
+%-----------------------------------------------------------------------
+% Job saved on 16-Mar-2022 09:47:13 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+% spm('defaults','fMRI');
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/Users/denicia/DEV/DEV_scripts/fMRI/fx/models/WTP/level2/prepostWTP_20230815/HealthyVSUnhealthy'};
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = {
+
+                                                          };
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'HealthyVSUnhealthy';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 1;
+matlabbatch{4}.spm.stats.results.spmmat(1) = cfg_dep('Contrast Manager: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{4}.spm.stats.results.conspec.titlestr = '';
+matlabbatch{4}.spm.stats.results.conspec.contrasts = 1;
+matlabbatch{4}.spm.stats.results.conspec.threshdesc = 'FWE';
+matlabbatch{4}.spm.stats.results.conspec.thresh = 0.05;
+matlabbatch{4}.spm.stats.results.conspec.extent = 0;
+matlabbatch{4}.spm.stats.results.conspec.conjunction = 1;
+matlabbatch{4}.spm.stats.results.conspec.mask.none = 1;
+matlabbatch{4}.spm.stats.results.units = 1;
+matlabbatch{4}.spm.stats.results.export{1}.ps = true;
+matlabbatch{4}.spm.stats.results.export{2}.fig = true;
+matlabbatch{4}.spm.stats.results.export{3}.png = true;
